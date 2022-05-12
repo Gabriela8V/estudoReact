7aea7a27cc4349e122a30ed506f15d574be24d38
GABRIELA VASCONCELOS DOS SANTOS <gabriela.vsantos13@senacsp.edu.br>
2022-05-12 15:31:40 -0300

    criação do projeto terceiro e execução do projeto

--- terceiro/src/App.js
+++ terceiro/src/App.js
@@ -1,18 +1,14 @@
+import { useState } from "react";
 function App() {
-  let valor1 = Math.round(Math.random() * 15);
-  let valor2 = Math.round(Math.random() * 15);
+  const [numero, setNumero] = useState(1);
 
   return (
     <div>
-      <p>A soma de 50 e 10 é {50 + 10}</p>
-      <p>O resto da divisão entre 50 e 3 é {50 % 3}</p>
-      <p>
-        A soma entre {valor1} e {valor2} é {valor1 + valor2}
-      </p>
-      <p>
-        O resultado da soma a cima é um número
-        {(valor1 + valor2) % 2 === 0 ? " Par" : " Impar"}
-      </p>
+      <p>O número atual é {numero}</p>
+      <button title="Clique" onClick={() => setNumero(numero + 1)}>
+        {" "}
+        Clique aqui{" "}
+      </button>
     </div>
   );
 }
